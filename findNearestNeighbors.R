e4a802d11a4594cac2c7fa0c8a4d60266a3fb30b
gwang37 <gwang37@jhu.edu>
2020-01-13 14:49:28 -0500

    modified find NN

--- findNearestNeighbors.R
+++ findNearestNeighbors.R
@@ -1,9 +1,9 @@
-findNearestNeighbors = function(maskImg, radius){
+findNearestNeighbors = function(maskImg, radius = 1){
   ## This function aims to find the approximate set of neighbors of each voxels. 
   ## The number of elements in each neighborhood is (2*radius)^3.
   
   ## maskImg: Input Masked image which masked voxels are labeled with zeros
-  ## radius: indices that dist(targetInd - queryInd) <= radius
+  ## radius:  Indices that dist(targetInd - queryInd) <= radius
   require(Rvcg)
   
   ind.mask <- which(maskImg>0, arr.ind = T)

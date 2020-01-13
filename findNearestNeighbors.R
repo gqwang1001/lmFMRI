ce6f5e227db8befcdfb7a8394576bcff22844be4
gwang37 <gwang37@jhu.edu>
2020-01-13 13:54:04 -0500

    add findNeearestNeighbors function

---
+++ findNearestNeighbors.R
@@ -0,0 +1,14 @@
+findNearestNeighbors = function(maskImg, radius){
+  ## This function aims to find the approximate set of neighbors of each voxels. 
+  ## The number of elements in each neighborhood is (2*radius)^3.
+  
+  ## maskImg: Input Masked image which masked voxels are labeled with zeros
+  ## radius: indices that dist(targetInd - queryInd) <= radius
+  require(Rvcg)
+  
+  ind.mask <- which(maskImg>0, arr.ind = T)
+  length.mask <- nrow(ind.mask)
+  
+  nn = Rvcg::vcgKDtree(target = ind.mask, query = ind.mask, k = radius^3*8, threads = 10)
+  return(nn)
+}
